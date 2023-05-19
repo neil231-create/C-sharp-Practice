912c5c4c4f0e65d3823a1a25a11d83590f893391
Tracy Neil Padilla <92151131+neil231-create@users.noreply.github.com>
2023-05-19 17:29:04 +0800

    Add files via upload

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myString = "cow";
+string mySecondString = "Cows";
+
+Console.WriteLine(myString);
+Console.WriteLine(mySecondString);
